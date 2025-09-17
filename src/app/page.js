f48cec87c3ab36d9be9fcc7b9d9d0538997ef6a4
HugoDemont <hugodemont62@hotmail.fr>
2025-09-17 12:10:09 +0200

    refactor Home component; change Header positioning to fixed for improved layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,8 @@ import Header from '@/blocks/Header';
 
 export default function Home() {
   return (
-    <div className="relative w-full h-screen">
-      <Header className="absolute top-0 left-0 w-full z-20" />
+    <div>
+      <Header className="fixed" />
       <Hero />
     </div>
   );
