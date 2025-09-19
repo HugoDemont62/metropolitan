40944a23b9a913fb347d2f97641e07e5f50922a2
HugoDemont <hugodemont62@hotmail.fr>
2025-09-19 14:38:30 +0200

    add Slider component and integrate it into the Home page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Hero from '@/blocks/Hero';
 import Header from '@/blocks/Header';
 import Footer from '@/blocks/Footer';
+import Slider from '@/blocks/Slider/Slider';
 
 export default function Home() {
   return (
     <div className="bg-gray-50">
       <Header/>
       <Hero/>
+      <Slider />
       <Footer/>
     </div>
   );
